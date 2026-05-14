decf28f3ac1154568e6c032b072ad96cc832a7b0
AMLameg <miguel.lameg23@gmail.com>
2026-05-14 11:44:54 +0100

    chargers

--- cesium-app/src/cesium.js
+++ cesium-app/src/cesium.js
@@ -16,6 +16,13 @@ const viewer = new Cesium.Viewer("cesiumContainer", {
 // Enable sky/atmosphere rendering
 viewer.scene.skyAtmosphere.show = true;
 
+// Init colours
+const hotpink_mod1 = Cesium.Color.HOTPINK.withAlpha(1.0);
+const hotpink = Cesium.Color.HOTPINK;
+const white = Cesium.Color.WHITE
+const red_mod1 = Color.RED.withAlpha(0.3);
+const red = Color.RED;
+
 try {
   const tileset = await Cesium.createGooglePhotorealistic3DTileset({
     onlyUsingWithGoogleGeocoder: true,
@@ -31,9 +38,6 @@ try {
   dataSource.entities.suspendEvents()
 
   const entities = dataSource.entities.values;
-  const hotpink_mod1 = Cesium.Color.HOTPINK.withAlpha(1.0);
-  const hotpink = Cesium.Color.HOTPINK;
-  const white = Cesium.Color.WHITE
   for (const entity of entities) {
 
     if (entity.polygon) {
@@ -62,15 +66,56 @@ try {
 }
 
 
-const roads = await Cesium.GeoJsonDataSource.load('../public/glasgow.geojson', {
-    stroke: Color.RED,
-    strokeWidth: 3,       
-    fill: Color.RED.withAlpha(0.3),     
-    clampToGround: true
+const roads = await Cesium.GeoJsonDataSource.load('../public/glasgow.geojson', {     
+    clampToGround: true,
+    infoBox: false
   });
 
+roads.entities.suspendEvents();
 
+const road_entities = roads.entities.values;
+road_entities.forEach((road) => {
+    road.stroke = red,
+    road.strokeWidth =  3,       
+    road.fill = red_mod1
+});
+roads.entities.resumeEvents();
 await viewer.dataSources.add(roads);
+
+const foo = await fetch('/../public/glasgow_chargers.csv')
+const evc_data = await foo.text()
+
+const rows = evc_data.split('\n').slice(1);
+
+rows.forEach((row) => {
+    const cols = row.split(',');
+    if (cols.length < 2) return; 
+
+    const lat          = parseFloat(cols[0]);
+    const lon          = parseFloat(cols[1]);
+    const id           = cols[2];
+    const power        = cols[3];
+    const connectorId  = cols[4];
+
+    viewer.entities.add({
+      name: `Charger: ${id}`,
+      position: Cesium.Cartesian3.fromDegrees(lon, lat),
+      point: {
+        pixelSize: 50,
+        color: Cesium.Color.ORANGE,
+        outlineColor: Cesium.Color.WHITE,
+        outlineWidth: 2,
+        heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
+      },
+
+      description: `
+        <table class="cesium-infoBox-defaultTable">
+          <tr><th>Power</th><td>${power} kW</td></tr>
+          <tr><th>Connector ID</th><td>${connectorId}</td></tr>
+        </table>
+      `
+    });
+  });
 
 // Fly to Glasgow
 viewer.camera.flyTo({
